aa50a5f4f5296b345e7ccd7e0a558a6534e0e4ef
theodora0422 <theodoracarare2004@gmail.com>
2026-04-18 11:19:55 +0300

    Only one property can be selected

--- trace_vizualizer/web_frontend/analysis_configuration_module/analysis_configuration_module.js
+++ trace_vizualizer/web_frontend/analysis_configuration_module/analysis_configuration_module.js
@@ -1,10 +1,21 @@
+function getSelectedProperty() {
+    const selectedOption = document.querySelector('input[name="analysis-property"]:checked');
+
+    if (!selectedOption) {
+        return null;
+    }
+
+    return selectedOption.value;
+}
+
 function getAnalysisConfiguration() {
+    const selectedProperty = getSelectedProperty();
+
     return {
-        check_deadlock: document.getElementById("deadlock").checked,
-        check_data_race: document.getElementById("data-race").checked,
-        check_starvation: document.getElementById("starvation").checked,
-        check_mutual_exclusion: document.getElementById("mutual-exclusion").checked,
-        max_depth: 10
+        check_deadlock: selectedProperty === "deadlock",
+        check_starvation: selectedProperty === "starvation",
+        check_data_race: selectedProperty === "data_race",
+        check_mutual_exclusion: selectedProperty === "mutual_exclusion"
     };
 }
 
